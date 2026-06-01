ffd9fcbe864d2fcc22ec85512130231ac3f8d7c5
Juho Jama <juho.jama@gmail.com>
2026-06-01 15:06:44 +0300

    Generate stubs for ACF PRO 6.6.2

--- acf-pro-stubs.php
+++ acf-pro-stubs.php
@@ -16,7 +16,7 @@ namespace {
          *
          * @var string
          */
-        public $version = '6.6.1';
+        public $version = '6.6.2';
         /**
          * The plugin settings array.
          *
